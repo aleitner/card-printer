d27e39f1c3db917ff77797b631c8816c5d300c72
Alexander Leitner <leitnersalex@gmail.com>
2023-06-10 15:37:19 -0400

    Add support for multiple card games

--- index.js
+++ index.js
@@ -2,35 +2,37 @@ const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
 
-fs.readFile('cards.json', 'utf-8', async (err, data) => {
-  if (err) throw err;
+(async () => {
+  // Asynchronously get the names of subdirectories in cards
+  const cardGames = await fs.promises.readdir('./cards', { withFileTypes: true })
+    .then(entries => entries.filter(entry => !entry.isDirectory()).map(entry => entry.name));
 
-  const games = JSON.parse(data);
+  // Process each card folder
+  for (const cardGameFile of cardGames) {
 
-  // Asynchronously get the names of subdirectories in templates
-  const subdirectories = await fs.promises.readdir('./templates', { withFileTypes: true })
-    .then(entries => entries.filter(entry => entry.isDirectory()).map(entry => entry.name));
+    const cardGame = path.parse(cardGameFile).name  
 
-  // Use Object.keys() to get an array of game names
-  Object.keys(games).forEach(gameName => {
-    const cards = games[gameName];
+    // Read the card JSON file
+    fs.readFile(`cards/${cardGameFile}`, 'utf-8', (err, data) => {
+      if (err) throw err;
 
-    // Find the case-insensitive match for the game template folder
-    const matchedFolder = subdirectories.find(folder => folder.toLowerCase() === gameName.toLowerCase());
+      const cards = JSON.parse(data);
 
-    if (matchedFolder) {
+      // Check if a matching template folder exists
+      if (fs.existsSync(`./templates/${cardGame}`)) {
         const gameTemplatePath = path.join(__dirname, 'templates', 'base_card_template.ejs');
-      
-        ejs.renderFile(gameTemplatePath, { cards, gameFolder: matchedFolder }, (err, html) => {
+
+        ejs.renderFile(gameTemplatePath, { cards, gameFolder: cardGame }, (err, html) => {
           if (err) throw err;
-      
-          fs.writeFile(`${gameName}_cards.html`, html, err => {
+
+          fs.writeFile(`${cardGame}_cards.html`, html, err => {
             if (err) throw err;
-            console.log(`${gameName}_cards.html has been created`);
+            console.log(`${cardGame}_cards.html has been created`);
           });
         });
       } else {
-        console.log(`No card template found for the ${gameName} game.`);
+        console.log(`No card template found for the ${cardGame} game.`);
       }
-  });
-});
+    });
+  }
+})();
